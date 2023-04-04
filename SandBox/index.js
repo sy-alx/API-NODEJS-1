2bc926f1a679f35bd6f0654ba0476c48cff52045
sy-alx <84022109+sy-alx@users.noreply.github.com>
2023-04-04 08:57:40 +0200

    PremierPushInit
    Premier push pour initialisation

---
+++ SandBox/index.js
@@ -0,0 +1,15 @@
+function maFunction(params){
+    return params * 2;
+}
+
+const maFunction = function (params) {
+    return params *2;
+}
+
+const maFunction = (params) => {
+    return params*2;
+}
+
+const maFunction = (params) => params*2
+
+const maFunction = params => params *2 
